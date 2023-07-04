1b7b9168631bd2f1be83ee18c92da65e635edbe5
rx0f <ja_louggani@esi.dz>
2023-07-04 13:14:55 +0200

    add: help command

---
+++ src/commands/general/help.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Displays the help of the bot"),
+  async execute(client, interaction, args) {
+    const message = new EmbedBuilder()
+      .setTitle("List of commands")
+      .addFields(
+        {
+          name: "`/help`",
+          value: "Displays the list of available commands",
+        },
+        {
+          name: "`/agenda`",
+          value: "Displays the event agenda",
+        },
+        {
+          name: "`/create_team` `<team_name>` `<member1>` `<member2>` `<member3>` `<member4>`",
+          value:
+            "Creates a team space for the team members entered as tags, the members parameters are optionnal",
+        },
+        {
+          name: "`ask_mentor`",
+          value: "Ask for a mentor by filling a form",
+        },
+        {
+          name: "`ask_organizer`",
+          value: "Ask for an organizer by filling a form",
+        },
+        {
+          name: "`/remaining_time`",
+          value:
+            "Checks the remaining time for the Game Jam until it starts/finishes (depending on when fired)",
+        },
+        {
+          name: "`/ping`",
+          value: "Gets the bot's ping",
+        }
+      )
+      .setColor(Colors.Purple)
+      .setThumbnail("https://i.imgur.com/o0CqYoh.png")
+
+    await interaction.reply({ embeds: [message] });
+  },
+};
