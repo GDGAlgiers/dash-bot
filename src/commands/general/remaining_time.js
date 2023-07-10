2286dad85a8fc534450e02b8c0ad7a865ac10d3b
rx0f <ja_louggani@esi.dz>
2023-07-10 15:07:19 +0200

    edit: end time

--- src/commands/general/remaining_time.js
+++ src/commands/general/remaining_time.js
@@ -16,7 +16,7 @@ module.exports = {
     });
 
     const startDate = new Date(2023, 6, 5, 18, 0);
-    const endDate = new Date(2023, 6, 10, 20, 0);
+    const endDate = new Date(2023, 6, 11, 0, 0);
     const currentDate = new Date();
     const message = new EmbedBuilder();
 
