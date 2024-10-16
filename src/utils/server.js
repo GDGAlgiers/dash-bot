0d0c829b53ea14e0da0473731f7a006d2536e5d3
mac <ninofcm20@gmail.com>
2024-10-16 01:42:26 +0100

    register dummy endpoint to test something

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 
+const getData = async (req, res) => {
+  try {
+    const data = req.body;
+
+    console.log("====================================");
+    console.log("data:", data);
+    console.log("====================================");
+
+    return res.send(data);
+  } catch (err) {
+  res.send(err);
+  }
+};
+
 function createServer() {
     const app = express();
     const port = process.env.PORT||3000;
   
     // Middleware to parse JSON
     app.use(express.json());
+
+    // Route registration for getData
+    app.post('/getdata', getData);
   
     // Start the server
     app.listen(port, () => {
